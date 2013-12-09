926706b40e3f1cd3a3889ac0919a353a7420759b
Seba Fraresso <seba@diproach.com>
2013-12-09 20:23:48 -0300

    Initial

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var myScroll;
+function loaded() {
+	myScroll = new iScroll('wrapper',{ hScrollbar: false, vScrollbar: false });
+}
+
+document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+
+/* * * * * * * *
+ *
+ * Use this for high compatibility (iDevice + Android)
+ *
+ */
+document.addEventListener('DOMContentLoaded', function () { setTimeout(loaded, 200); }, false);
+/*
+ * * * * * * * */
+
+
+/* * * * * * * *
+ *
+ * Use this for iDevice only
+ *
+ */
+//document.addEventListener('DOMContentLoaded', loaded, false);
+/*
+ * * * * * * * */
+
+
+/* * * * * * * *
+ *
+ * Use this if nothing else works
+ *
+ */
+//window.addEventListener('load', setTimeout(function () { loaded(); }, 200), false);
+/*
+ * * * * * * * */
+
+ window.mySwipe = Swipe(document.getElementById('slider'));
